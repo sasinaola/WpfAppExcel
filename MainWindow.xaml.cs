6b5f3c34c05cd519e3364325fc016c97adf1e5b0
Sina <Sina@PC-IA>
2022-10-04 11:39:40 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Office.Interop;
+using ExcelAdaptorLib = Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+using ExcelAdaptorLib;
+
+namespace WpfAppExcel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Excel.Application objExcel = new Excel.Application();
+            objExcel.Visible = true;
+        }
+    }
+}
